a72ea657c5448109f5a15d95e5ce57987c6b290f
Kris <krijojo@gmail.com>
2019-11-09 15:28:47 +0100

    Added first draft of custom physics (basic movement + hanging on stars). This temporarily closes issue #13. Must be re-opened in case we choose to go on implementing custom physics

---
+++ Prototype_1/Assets/Scripts/CustomPhysics/CustomGrappler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace CustomPhysics
+{
+    public class CustomGrappler : MonoBehaviour
+    {
+        public HangingEffect hangingEffect;
+        private CharacterController2D _controller;
+        private CustomLocker _locker;
+        private Rigidbody2D _rb;
+        private bool _hook;
+        private DistanceJoint2D _joint;
+        private FrictionJoint2D _friction;
+
+        private void Awake()
+        {
+            _controller = GetComponent<CharacterController2D>();
+            _locker = GetComponent<CustomLocker>();
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void HookInput()
+        {
+            if (Input.GetButtonDown("LockStarHang"))
+            {
+                _hook = true;
+            }
+            else if (Input.GetButtonUp("LockStarHang"))
+            {
+                _hook = false;
+            }
+        }
+
+        private void DestroyJoint()
+        {
+            _joint.connectedBody.gameObject.layer = 10;
+            _controller.SwitchToKinematic();
+            Destroy(_joint);
+            Destroy(_friction);
+            _joint = null;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            HookInput();
+            // If the player is hanged to a star and either he's not pressing the hook button or he's touching the ground,
+            // destroy the joint
+            if (_joint != null)
+            {
+                if (!_hook || _controller.IsGrounded())
+                {
+                    DestroyJoint();
+                    _locker.SetHanging(false);
+                    //hangingEffect.StopEffect();
+                }
+            }
+            // If the player is locking a star, he's not touching the ground, he's pressing the hook button
+            // and the joint does not exist yet, then create it
+            else
+            {
+                if (_locker.IsLocked() && !_controller.IsGrounded() && _hook)
+                {
+                    _joint = gameObject.AddComponent<DistanceJoint2D>();
+                    _friction = gameObject.AddComponent<FrictionJoint2D>();
+                    _rb.isKinematic = false;
+                    Rigidbody2D otherRb = _locker.GetTarget().GetComponent<Rigidbody2D>();
+                    _joint.distance = (_rb.position - otherRb.position).magnitude;
+                    _joint.connectedBody = otherRb;
+                    _friction.maxForce = 1f;
+                    _locker.GetTarget().layer = 0;
+                    _locker.SetHanging(true);
+                    //hangingEffect.StartEffect(otherRb.transform);
+                }
+            }
+        }
+    }
+}
---
+++ Prototype_1/Assets/Scripts/CustomPhysics/CustomLocker.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomPhysics{
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class CustomLocker : MonoBehaviour
+	{
+		public GameObject activeIcon;
+		public LayerMask starLayerMask;
+
+		private Vector2 _oldMousePosition;
+		private bool _locked;
+		private bool _hanging;
+		private GameObject _target;
+		private GameObject[] _stars;
+		private float _distance;
+		private Rigidbody2D _rb;
+
+		public Transform starViewFinder;
+
+		void Awake()
+		{
+			_rb = GetComponent<Rigidbody2D>();
+		}
+
+		private void LockInput()
+		{
+			if (Input.GetButtonDown("LockStarHang"))
+			{
+				_locked = true;
+			}
+			else if (Input.GetButtonUp("LockStarHang"))
+			{
+				_locked = false;
+			}
+		}
+
+		void Update()
+		{
+			LockInput();
+
+			if (_locked && !_hanging)
+			{
+				TargetStar();
+				if (_target != null)
+				{
+					activeIcon.transform.position = _target.transform.position;
+					activeIcon.SetActive(true);
+				}
+			}
+			else if (!_hanging)
+			{
+				_target = null;
+				activeIcon.SetActive(false);
+			}
+
+		}
+
+
+		private void TargetStar()
+		{
+			// Get mouse position as a Vector2
+			Vector2 mousePosition = starViewFinder.position;
+
+			// Cast a ray from player to mouse position
+			Vector2 relativeMousePos = mousePosition - _rb.position;
+			RaycastHit2D hit = Physics2D.Raycast(_rb.position, relativeMousePos, Mathf.Infinity, starLayerMask);
+
+			pos = relativeMousePos * 100;
+			if (hit.rigidbody == null)
+			{
+				activeIcon.SetActive(false);
+				return;
+			}
+
+			_target = hit.rigidbody.gameObject;
+		}
+
+		// START of GIZMOS section useful for debugging
+
+		private Vector2 pos;
+//
+//    private void OnDrawGizmos()
+//    {
+//	    if(pos != null)
+//			Gizmos.DrawRay(_rb.position, pos);
+//    }
+//    
+		// END of GIZMOS section
+
+		public bool IsLocked()
+		{
+			return _locked;
+		}
+
+		public GameObject GetTarget()
+		{
+			return _target;
+		}
+
+		public void SetHanging(bool hanging)
+		{
+			_hanging = hanging;
+		}
+	}
+}
