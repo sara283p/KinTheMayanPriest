668620cde55b2afe4c9dc5222e95e03454ffef82
Kris <krijojo@gmail.com>
2019-11-15 12:06:05 +0100

    Code cleaning and refactoring. Added comments to clearly understand code behavior

--- Prototype_1/Assets/Scripts/NewCharacterController.cs
+++ Prototype_1/Assets/Scripts/NewCharacterController.cs
@@ -5,42 +5,32 @@ using UnityEngine.Events;
 public class NewCharacterController : MonoBehaviour
 
 {
+	public Transform _leftJumpCheck;											// Position markings used to check when the character has landed after jumps
+	public Transform _rightJumpCheck;
+	public Transform floorCheck;												// A position marking used to check the direction of the floor in front of the character
+
+	
 	[SerializeField] private float _jumpForce = 400f;							// Amount of force added when the player jumps.
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
 	[SerializeField] private bool _airControl = false;							// Whether or not a player can steer while jumping;
 	[SerializeField] private LayerMask _whatIsGround;							// A mask determining what is ground to the character
-	private ContactFilter2D _contactFilter;
-	[SerializeField] private Transform _groundCheck;
-
-	public Transform _leftJumpCheck;
-	public Transform _rightJumpCheck;
-	
-	// A position marking where to check if the player is grounded.
-	private float _oscillationSpeed;
-	[SerializeField] private float _oscillationDelta;
-	[SerializeField] private float _baseOscillationSpeed;
-	private Vector2 _groundNormal;
-	private Vector2 _currentNormal;
-	private Vector2 _starPosition;
-	private Collider2D[] _collArray = new Collider2D[1];
-	
-	private const float _groundedRadius = 0.2f; // Radius of the overlap circle to determine if grounded
-	[SerializeField] private bool _grounded;            // Whether or not the player is grounded.
-	private bool _hooked;
-	private Rigidbody2D _rb;
-	private Collider2D _collider;
-	private bool _facingRight = true;  // For determining which way the player is currently facing.
-	private Vector3 _velocity = Vector3.zero;
-	public Transform floorCheck;
-	[SerializeField] private bool _jumping;
+	[SerializeField] private Transform _groundCheck;  							// A position marking where to check if the player is grounded.
+	private float _oscillationSpeed;											// Max oscillation speed used to avoid circles while hanging
+	[SerializeField] private float _oscillationDelta;							// Delta of space the player performs while oscillating
+	[SerializeField] private float _baseOscillationSpeed;						// Base oscillation speed used to compute the maximum oscillation speed according to distance from the star
+	private Vector2 _groundNormal;												// Vector in normal direction w.r.t. the ground
+	private Vector2 _starPosition;												// Position of the star the player is hooked to
+	private const float _groundedRadius = 0.2f; 								// Distance from ground at which player is considered to be grounded
+	[SerializeField] private bool _grounded;            						// Whether or not the player is grounded.
+	private bool _hooked;														// Whether or not the player is hanged to a star
+	private Rigidbody2D _rb;													// Character's rigidbody component
+	private bool _facingRight = true; 											// For determining which way the player is currently facing.
+	private Vector3 _velocity = Vector3.zero;									// Velocity computed by SmoothDamp method
+	[SerializeField] private bool _jumping;										// Whether or not the player has jumped
 	
 	private void Awake()
 	{
 		_rb = GetComponent<Rigidbody2D>();
-		_collider = GetComponent<CircleCollider2D>();
-		_contactFilter = new ContactFilter2D();
-		_contactFilter.SetLayerMask(_whatIsGround);
-		_contactFilter.useLayerMask = true;
 		_groundNormal = new Vector2(0, 1);
 	}
 
@@ -48,8 +38,7 @@ public class NewCharacterController : MonoBehaviour
 	{
 		_grounded = false;
 		
-		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		// Check if player has landed after jumping
 		RaycastHit2D hit = Physics2D.Raycast(_leftJumpCheck.position, Vector2.down, 0.015f, _whatIsGround);
 		Vector2 newPos = _rb.position;
 		if (hit.collider)
@@ -65,11 +54,13 @@ public class NewCharacterController : MonoBehaviour
 
 			}
 		}
+		// Check whether the player is on the ground
 		hit = Physics2D.Raycast(_groundCheck.position, Vector2.down, _groundedRadius, _whatIsGround);
 		if (hit.collider)
 		{
 			_grounded = true;
 		}
+		// Compute floor direction
 		hit = Physics2D.Raycast(floorCheck.position, Vector2.down, Mathf.Infinity, _whatIsGround);
 		if (hit.collider)
 		{
@@ -103,6 +94,7 @@ public class NewCharacterController : MonoBehaviour
 		{
 			// Move the character by finding the target velocity
 			Vector3 targetVelocity;
+			// If the player is not hanged on a star...
 			if (!_hooked)
 			{
 				Vector2 moveAlongGround = new Vector2(_groundNormal.y, - _groundNormal.x);
@@ -113,22 +105,21 @@ public class NewCharacterController : MonoBehaviour
 					targetVelocity.y = -_rb.velocity.y;
 					
 				}
-				// If player is falling or it is moving along a ramp, keep it's vertical speed
-				/*if (!_grounded || _currentNormal == _groundNormal)
-					targetVelocity.y = _rb.velocity.y;*/
-
-				// And then smoothing it out and applying it to the character
 			}
+			// ... otherwise...
 			else
 			{
 				targetVelocity = _rb.velocity;
+				// If player is moving horizontally, increase oscillation...
 				if(Math.Abs(move) > 0.01f)
 						targetVelocity.x += Math.Sign(move) * _oscillationDelta;
 
+				//... but if oscillation speed is too high, set it to a maximum speed to avoid performing entire circles
 				if (Math.Abs(targetVelocity.x) > _oscillationSpeed)
 					targetVelocity.x = _oscillationSpeed * Math.Sign(targetVelocity.x);
 			}
 			
+			// Set rigidbody velocity to move player
 			_rb.velocity = Vector3.SmoothDamp(_rb.velocity, targetVelocity, ref _velocity, m_MovementSmoothing);
 
 
@@ -149,7 +140,7 @@ public class NewCharacterController : MonoBehaviour
 		// If the player should jump...
 		if (_grounded && jump)
 		{
-				// If the player is not on a plain platform (avoids rocket jumps on ramps)
+				// If the player is not on a plain platform and wants to jump avoid rocket jump
 				if (_rb.velocity.y > 0.01)
 					_rb.velocity = new Vector2(_rb.velocity.x, 0);
 				_jumping = true;
@@ -176,6 +167,7 @@ public class NewCharacterController : MonoBehaviour
 	public void ToggleHook()
 	{
 		_hooked = !_hooked;
+		// If player is hanging to a star, set the maximum oscillation speed according to the distance from the star
 		if (_hooked)
 		{
 			_starPosition = _rb.GetComponent<DistanceJoint2D>().connectedBody.position;
