804778e3e7d6a8b6c909383bd8a74ac6a5b4bdec
xXEmilioXx <38227115+xXEmilioXx@users.noreply.github.com>
2019-11-17 19:22:49 +0100

    Add player's health

--- Prototype_1/Assets/Scripts/Enemy.cs
+++ Prototype_1/Assets/Scripts/Enemy.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public int health = 100;
+    public float damageToPlayer = 20f;
 
     //public GameObject deathEffect;
 
@@ -22,5 +24,10 @@ public class Enemy : MonoBehaviour
     {
         //Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damageToPlayer);
     }
 }
---
+++ Prototype_1/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public float max_health = 100f;
+    public float cur_health = 0f;
+    public bool alive = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        alive = true;
+        cur_health = max_health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (!alive)
+        {
+            return;
+        }
+
+        if (cur_health - amount <= 0)
+        {
+            cur_health = 0;
+            alive = false;
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            cur_health -= amount;
+        }
+    }
+}
