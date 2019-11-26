6bda1689dd61eb68fd74703004e212a3b22abf78
Kris <krijojo@gmail.com>
2019-11-26 16:33:46 +0100

    Camera Management scripts loaded + LevelDesign_02 filled with colliders, player and cameras

---
+++ Prototype_1/Assets/Scripts/CameraManagement/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private static CinemachineConfiner _confiner;
+
+    private void Awake()
+    {
+        _confiner = GetComponent<CinemachineConfiner>();
+    }
+
+    public static void ChangeCamera(GameObject toDisable, GameObject toEnable)
+    {
+        toDisable.SetActive(false);
+        CinemachineVirtualCamera asdf = toEnable.GetComponentInChildren<CinemachineVirtualCamera>(true);
+        asdf.gameObject.SetActive(true);
+    }
+}
