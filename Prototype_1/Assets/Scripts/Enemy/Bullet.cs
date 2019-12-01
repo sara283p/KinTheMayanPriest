d0e569c09e1bcdc9aa4c8a875cf51a7f602a6450
xXEmilioXx <38227115+xXEmilioXx@users.noreply.github.com>
2019-12-01 19:00:14 +0100

    Add shooting enemy

---
+++ Prototype_1/Assets/Scripts/Enemy/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public float damageToPlayer = 20f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.isTrigger)
+        {
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damageToPlayer);
+        }
+        //the bullet has to be destroyed no matter what it hit
+        Destroy(gameObject);
+    }
+}
---
+++ Prototype_1/Assets/Scripts/Enemy/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float firingInterval = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Shoot");
+    }
+    
+    
+
+
+    IEnumerator Shoot()
+    {
+        while (true)
+        {
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            yield return new WaitForSeconds(firingInterval);
+        }
+    }
+    
+    void SSShootdd()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
