f537243b88d9b863005821f66c16899da84f4064
rpigozzi <riccardo.pigozzi@mail.polimi.it>
2019-12-03 17:59:44 +0100

    Added exit level UI (with yet to be completed scripts)

---
+++ Prototype_1/Assets/Scripts/UI/ExitLevel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitLevel : MonoBehaviour
+{
+    private GameObject yesMarker;
+    private GameObject noMarker;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject yesMarker = GameObject.FindGameObjectWithTag("YesMarker");
+        GameObject noMarker = GameObject.FindGameObjectWithTag("NoMarker");
+        
+        yesMarker.GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (yesMarker.GetComponent<Renderer>().enabled == false && InputManager.GetAxis("Horizontal") < -0.01)
+        {
+            yesMarker.GetComponent<Renderer>().enabled = true;
+            noMarker.GetComponent<Renderer>().enabled = false;
+        }
+        
+        else if (noMarker.GetComponent<Renderer>().enabled == false && InputManager.GetAxis("Horizontal") > 0.01)
+        {
+            noMarker.GetComponent<Renderer>().enabled = true;
+            yesMarker.GetComponent<Renderer>().enabled = false;
+        }
+
+        else
+        {
+            return;
+        }
+    }
+}
