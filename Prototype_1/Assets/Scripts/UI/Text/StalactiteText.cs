9690f76fdd6ebb835ff7cf77765d5de7216264c4
xXEmilioXx <38227115+xXEmilioXx@users.noreply.github.com>
2019-12-05 19:44:10 +0100

    Create text for approaching stalactite

---
+++ Prototype_1/Assets/Scripts/UI/Text/StalactiteText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalactiteText : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.isTrigger)
+        {
+            EventManager.TriggerEvent("StalactiteApproaching");
+        }
+        
+    }
+}
---
+++ Prototype_1/Assets/Scripts/UI/Text/TextManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+
+    public Text stalactiteText;
+    [SerializeField] private float timeToAppear = 2f;
+    private float timeWhenDisappear;
+
+    
+    
+    void Update()
+    {
+        if (stalactiteText.enabled && (Time.time >= timeWhenDisappear))
+        {
+            stalactiteText.enabled = false;
+        }
+    }
+    
+    public void EnableStalactiteText()
+    {
+        stalactiteText.enabled = true;
+        timeWhenDisappear = Time.time + timeToAppear;
+    }
+    
+    private void OnEnable()
+    {
+        EventManager.StartListening("StalactiteApproaching", EnableStalactiteText);
+    }
+    
+    private void OnDisable()
+    {
+        EventManager.StopListening("StalactiteApproaching", EnableStalactiteText);
+    }
+    
+}
