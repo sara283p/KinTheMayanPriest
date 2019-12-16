f88e560292dee1cb26e96c56ef165a932f372069
Alessandro Falcetta <alessandro.falcetta@mail.polimi.it>
2019-12-16 17:05:13 +0100

    Fix for obstacles between stars

--- Prototype_1/Assets/Scripts/Character/HookingAndAttack_Joystick/Attack_Joystick.cs
+++ Prototype_1/Assets/Scripts/Character/HookingAndAttack_Joystick/Attack_Joystick.cs
@@ -172,6 +172,20 @@ public class Attack_Joystick : MonoBehaviour
                 return;
             }
 
+            for (int i = 0; i < _selectedStars.Count - 1; i++)
+            {
+                relativePosition = _selectedStars[i + 1].transform.position - _selectedStars[i].transform.position;
+                if (Physics2D.Raycast(_selectedStars[i].transform.position, relativePosition,
+                    relativePosition.magnitude,
+                    obstacleLayerMask))
+                {
+                    lineRenderer.positionCount = 0;
+                    Abort();
+                    return;
+                }
+
+            }
+
             var starsWentUnavailable = _selectedStars.Where(x => x.isDisabled).ToList();
             if (starsWentUnavailable.Count > 0)
             {
@@ -259,6 +273,17 @@ public class Attack_Joystick : MonoBehaviour
             // or deselect a star already selected, so remove it, deselect it, and redraw the animation.
             if (!_targetStar.IsSelectedForAttack())
             {
+                var lastStar = _selectedStars.LastOrDefault();
+                if (lastStar)
+                {
+                    Vector2 relativePosition = _targetStar.transform.position - lastStar.transform.position;
+                    if (Physics2D.Raycast(lastStar.transform.position,
+                        relativePosition, relativePosition.magnitude,
+                        obstacleLayerMask))
+                    {
+                        return;
+                    }
+                }
                 _targetStar.SelectForAttack();
                 _selectedStars.Add(_targetStar);
                 lineRenderer.positionCount++;
