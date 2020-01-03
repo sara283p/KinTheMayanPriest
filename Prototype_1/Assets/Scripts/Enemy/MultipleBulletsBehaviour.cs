449170d36dd7e1ca5574f097482dfa19d9c48726
rpigozzi <riccardo.pigozzi@mail.polimi.it>
2020-01-03 21:45:43 +0100

    Added first wave shooting enemy as example, plus fixes to enemy AI in BetaLevel_3

---
+++ Prototype_1/Assets/Scripts/Enemy/MultipleBulletsBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleBulletsBehaviour : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public float damageToPlayer = 20f;
+
+    public GameObject shooter;
+
+    [SerializeField] private float timeToLast = 3f;
+    private float timeWhenGetDestroyed;
+    
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+        timeWhenGetDestroyed = Time.time + timeToLast;
+    }
+
+    private void Update()
+    {
+        if (Time.time >= timeWhenGetDestroyed)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && other.isTrigger)
+        {
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damageToPlayer);
+            Destroy(gameObject);
+        }
+
+        else if (other.CompareTag("Bullet") && other.isTrigger)
+        {
+            Physics.IgnoreCollision(GetComponent<Collider>(), other.GetComponent<Collider>());
+        }
+        
+        else if (other.CompareTag("Enemy") && other.isTrigger)
+        {
+            Physics.IgnoreCollision(GetComponent<Collider>(), other.GetComponent<Collider>());
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
