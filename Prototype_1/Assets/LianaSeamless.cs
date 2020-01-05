2cbba334aacf03791e4804ccfc736ce67dde8456
Alessandro Falcetta <alessandro.falcetta@mail.polimi.it>
2020-01-05 12:40:17 +0100

    Improvements level3

---
+++ Prototype_1/Assets/LianaSeamless.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LianaSeamless : Health, IDamageable
+{
+    private LianaSeamlessPiece[] _children;
+    private Enemy[] _connectedEnemies;
+    private float _maxHealth;
+    private float _currentHealth;
+    private float _aliveChildren;
+    private Transform _targetPosition;
+
+    void Start()
+    {
+        _maxHealth = GameManager.Instance.obstacleMaxHealth;
+        _currentHealth = _maxHealth;
+        _targetPosition = GetComponentInChildren<LianaSeamlessTarget>().transform;
+        _children = GetComponentsInChildren<LianaSeamlessPiece>();
+        _connectedEnemies = GetComponentsInChildren<Enemy>();
+
+        foreach (var enemy in _connectedEnemies)
+        {
+            enemy.gameObject.SetActive(false); 
+        }
+    }
+    
+    public override float GetHealth()
+    {
+        return _currentHealth;
+    }
+
+    public override float GetMaxHealth()
+    {
+        return _maxHealth;
+    }
+    
+    public void TakeDamage(float damage)
+    {
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+        {
+            DestroyObstacle();
+        }
+    }
+
+    public Vector2 GetPosition()
+    {
+        return _targetPosition.position;
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+
+    private void DestroyObstacle()
+    {
+        foreach (var go in _children)
+        {
+            Destroy(go.gameObject);
+        }
+
+        GetComponent<Collider2D>().enabled = false;
+        
+        foreach (var enemy in _connectedEnemies)
+        {
+            enemy.gameObject.SetActive(true); 
+        }
+        
+        Destroy(this);
+    }
+
+}
