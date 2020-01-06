c116bc20376eec7001eed9d718b1a7faf5a24687
xXEmilioXx <38227115+xXEmilioXx@users.noreply.github.com>
2020-01-06 18:47:31 +0100

    Add fade in to tutorial text in level 1

--- Prototype_1/Assets/Scripts/TutorialManager.cs
+++ Prototype_1/Assets/Scripts/TutorialManager.cs
@@ -8,12 +8,20 @@ public class TutorialManager : MonoBehaviour
     [SerializeField] private GameObject channelLight;
     [SerializeField] private GameObject aimTarget;
     [SerializeField] private GameObject releaseEnergy;
+    private SpriteRenderer rendAimTarget;
+    private SpriteRenderer rendReleaseEnergy;
 
     private void Start()
     {
-        channelLight.SetActive(true);
-        aimTarget.SetActive(false);
-        releaseEnergy.SetActive(false);
+        rendAimTarget = aimTarget.GetComponent<SpriteRenderer>();
+        Color cAimTarget = rendAimTarget.material.color;
+        cAimTarget.a = 0f;
+        rendAimTarget.material.color = cAimTarget;
+
+        rendReleaseEnergy = releaseEnergy.GetComponent<SpriteRenderer>();
+        Color cReleaseEnergy = rendReleaseEnergy.material.color;
+        cReleaseEnergy.a = 0f;
+        rendReleaseEnergy.material.color = cReleaseEnergy;
     }
 
     private void OnEnable()
@@ -23,14 +31,36 @@ public class TutorialManager : MonoBehaviour
 
     private void ShowAimTarget()
     {
-        aimTarget.SetActive(true);
+        StartCoroutine("FadeInAimTarget");
         EventManager.StartListening("TargetAcquired", ShowReleaseEnergy);
         EventManager.StopListening("StarSelected", ShowAimTarget);
     }
 
     private void ShowReleaseEnergy()
     {
-        releaseEnergy.SetActive(true);
+        StartCoroutine("FadeInReleaseEnergy");
         EventManager.StopListening("TargetAcquired", ShowReleaseEnergy);
+    }
+
+    IEnumerator FadeInAimTarget()
+    {
+        for (float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color c = rendAimTarget.material.color;
+            c.a = f;
+            rendAimTarget.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    
+    IEnumerator FadeInReleaseEnergy()
+    {
+        for (float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color c = rendReleaseEnergy.material.color;
+            c.a = f;
+            rendReleaseEnergy.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }
