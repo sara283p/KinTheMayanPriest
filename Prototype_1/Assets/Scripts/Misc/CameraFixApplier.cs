02e6e4f983a21be6b8ece774ac74985d070d4c6c
Kris <krijojo@gmail.com>
2020-01-07 04:02:51 +0100

    Camera re-initiated on death + hud moved with camera

---
+++ Prototype_1/Assets/Scripts/Misc/CameraFixApplier.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class CameraFixApplier : MonoBehaviour
+{
+    public float offsetX;
+    public float offsetY;
+    
+    private GameObject _healthBar;
+    private GameObject _powerBar;
+    private float _distance;
+    private CinemachineVirtualCamera _virtualCamera;
+    private Camera _camera;
+    private float _initialOrthographicSize;
+    private float _initialScreenX;
+    private float _initialScreenY;
+    private CinemachineFramingTransposer _transposer;
+
+    private void Awake()
+    {
+        _healthBar = GetComponentInChildren<HealthBar>().gameObject;
+        _powerBar = GetComponentInChildren<PowerBar>().gameObject;
+        _virtualCamera = FindObjectsOfType<CinemachineVirtualCamera>().First(cam => cam.gameObject.activeInHierarchy);
+        _camera = FindObjectOfType<Camera>();
+        _initialOrthographicSize = _virtualCamera.m_Lens.OrthographicSize;
+        _transposer = _virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
+        _initialScreenX = _transposer.m_ScreenX;
+        _initialScreenY = _transposer.m_ScreenY;
+        _distance = ((Vector2) (_healthBar.transform.position - _powerBar.transform.position)).magnitude;
+    }
+
+    private void ReinitCamera()
+    {
+        _virtualCamera.m_Lens.OrthographicSize = _initialOrthographicSize;
+        _transposer.m_ScreenX = _initialScreenX;
+        _transposer.m_ScreenY = _initialScreenY;
+    }
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("PlayerDeath", ReinitCamera);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening("PlayerDeath", ReinitCamera);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 topLeft = _camera.ScreenToWorldPoint(new Vector3(0, Screen.height));
+        topLeft.x += offsetX;
+        topLeft.y -= offsetY;
+        topLeft.z = 0;
+
+        _healthBar.transform.position = topLeft;
+        topLeft.y -= _distance;
+        _powerBar.transform.position = topLeft;
+    }
+}
