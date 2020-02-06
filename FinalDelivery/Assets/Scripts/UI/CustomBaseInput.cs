99bb63a6848837dc8bb4040d39be0b2fcf689e27
Kris <krijojo@gmail.com>
2020-02-06 12:42:12 +0100

    Level selection now working with every combination OS-Controller (it uses the custom InputManager script, instead of the default one of Unity)

---
+++ FinalDelivery/Assets/Scripts/UI/CustomBaseInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomBaseInput : BaseInput
+{
+    public override float GetAxisRaw(string axisName)
+    {
+        return InputManager.GetAxisRaw(axisName);
+    }
+
+    public override bool GetButtonDown(string buttonName)
+    {
+        return InputManager.GetButtonDown(buttonName);
+    }
+}
---
+++ FinalDelivery/Assets/Scripts/UI/CustomStandaloneInputModule.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.EventSystems;
+
+namespace UI
+{
+    public class CustomStandaloneInputModule : StandaloneInputModule
+    {
+        protected override void Awake()
+        {
+            inputOverride = GetComponent<BaseInput>();
+        }
+    }
+}
