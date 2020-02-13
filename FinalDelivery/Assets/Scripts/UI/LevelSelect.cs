216972628f28dfaedbbee71a01638b1d881122d3
Kris <krijojo@gmail.com>
2020-02-13 13:48:49 +0100

    Updated menu buttons function + added possibility to get back to main menu from level selection with B

--- FinalDelivery/Assets/Scripts/UI/LevelSelect.cs
+++ FinalDelivery/Assets/Scripts/UI/LevelSelect.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
@@ -9,5 +11,13 @@ public class LevelSelect : MonoBehaviour
    {
       GameManager.Instance.ChangeLevel(levelName);
       Time.timeScale = 1f;
+   }
+
+   private void Update()
+   {
+      if (InputManager.GetButtonDown("Button1"))
+      {
+         SceneManager.LoadScene("Scenes/SavedDataMenu");
+      }
    }
 }
