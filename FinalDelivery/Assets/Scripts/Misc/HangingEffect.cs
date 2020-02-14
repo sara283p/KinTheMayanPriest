82e5a65b2e41285963c577cf80dd4291dbff3674
Kris <krijojo@gmail.com>
2020-02-14 12:47:53 +0100

    Changed colors of rays and viewfinder according to player's actions (either attacking or hanging)

--- FinalDelivery/Assets/Scripts/Misc/HangingEffect.cs
+++ FinalDelivery/Assets/Scripts/Misc/HangingEffect.cs
@@ -11,8 +11,12 @@ public class HangingEffect : MonoBehaviour
 
     public LineRenderer lineRenderer;
 
+    public Sprite normalViewFinder;
+    public Sprite hangingViewFinder;
+
     private GameObject _blueSphere;
     private Animator _blueSphereAnimator;
+    private SpriteRenderer _viewfinderSpriteRenderer;
     
     private static readonly int IsHanged = Animator.StringToHash("IsHanged");
 
@@ -21,6 +25,9 @@ public class HangingEffect : MonoBehaviour
         _blueSphere = transform.parent.GetComponentInChildren<HangBlueSphere>().gameObject;
         _blueSphereAnimator = _blueSphere.GetComponent<Animator>();
         _blueSphere.SetActive(false);
+        _viewfinderSpriteRenderer = transform.parent.parent
+            .GetComponentInChildren<MoveStarViewfinder_Joystick>()
+            .GetComponent<SpriteRenderer>();
     }
 
     public void StartEffect(Transform target)
@@ -30,6 +37,7 @@ public class HangingEffect : MonoBehaviour
         lineRenderer.enabled = true;
         _blueSphere.SetActive(true);
         _blueSphereAnimator.SetBool(IsHanged, true);
+        _viewfinderSpriteRenderer.sprite = hangingViewFinder;
     }
 
     public void StopEffect()
@@ -37,6 +45,7 @@ public class HangingEffect : MonoBehaviour
         lineRenderer.enabled = false;
         _blueSphereAnimator.SetBool(IsHanged, false);
         _blueSphere.SetActive(false);
+        _viewfinderSpriteRenderer.sprite = normalViewFinder;
     }
 
     private void UpdateLineRendererPosition()
