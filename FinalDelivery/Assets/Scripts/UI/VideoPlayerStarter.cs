76328bfe908002d5a8b5b0296a1d4c5180c23fa6
Kris <krijojo@gmail.com>
2020-02-14 15:50:41 +0100

    Menus with background looping video

---
+++ FinalDelivery/Assets/Scripts/UI/VideoPlayerStarter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerStarter : MonoBehaviour
+{
+    public GameObject toEnable;
+    
+    private VideoPlayer _videoPlayer;
+    
+    private void Awake()
+    {
+        if(toEnable)
+            toEnable.SetActive(false);
+        _videoPlayer = GetComponent<VideoPlayer>();
+        _videoPlayer.Prepare();
+        StartCoroutine(PlaybackStart());
+    }
+
+    private IEnumerator PlaybackStart()
+    {
+        while (!_videoPlayer.isPrepared)
+        {
+            yield return null;
+        }
+        _videoPlayer.Play();
+        if(toEnable)
+            toEnable.SetActive(true);
+
+    }
+}
