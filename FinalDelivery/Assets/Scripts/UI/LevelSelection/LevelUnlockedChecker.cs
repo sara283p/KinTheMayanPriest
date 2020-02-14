f3bfcb80192896b8c50c1dbc642e94b5abeb99a9
Kris <krijojo@gmail.com>
2020-02-14 16:04:44 +0100

    Locked levels shown, icons replaced with locked level icons

--- FinalDelivery/Assets/Scripts/UI/LevelSelection/LevelUnlockedChecker.cs
+++ FinalDelivery/Assets/Scripts/UI/LevelSelection/LevelUnlockedChecker.cs
@@ -2,16 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelUnlockedChecker : MonoBehaviour
 {
     public String levelName;
+    public Sprite lockedLevelIcon;
+
+    private GameObject _dotPath;
 
     private void Awake()
     {
+        _dotPath = GetComponentInChildren<DotPath>().gameObject;
         if (!GameManager.Instance.IsLevelUnlocked(levelName))
         {
-            gameObject.SetActive(false);
+            GetComponent<Image>().sprite = lockedLevelIcon;
+            GetComponent<Button>().enabled = false;
+            _dotPath.SetActive(false);
         }
     }
 }
