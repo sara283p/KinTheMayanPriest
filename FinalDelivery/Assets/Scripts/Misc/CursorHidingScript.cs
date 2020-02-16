cf9e65231ab9195d8f354c27eb0ce439d8b91473
Kris <krijojo@gmail.com>
2020-02-16 19:12:52 +0100

    Cursor hidden before splash screen

---
+++ FinalDelivery/Assets/Scripts/Misc/CursorHidingScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorHidingScript
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    static void OnBeforeSplashScreenLoadRuntimeMethod()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+}
