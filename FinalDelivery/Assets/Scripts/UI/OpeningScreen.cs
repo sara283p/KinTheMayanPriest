1bd7308182c16cad9abfca2d72592c02a9292daa
Kris <krijojo@gmail.com>
2020-02-18 13:08:06 +0100

    Ignore mouse button presses in OpeningScreen

--- FinalDelivery/Assets/Scripts/UI/OpeningScreen.cs
+++ FinalDelivery/Assets/Scripts/UI/OpeningScreen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -7,16 +8,49 @@ using UnityEngine.SceneManagement;
 public class OpeningScreen : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
-    
+    private KeyCode[] _keys;
+    private void Awake()
+    {
+        _keys = new KeyCode[7];
+        
+        _keys[0] = KeyCode.Mouse0;
+        _keys[1] = KeyCode.Mouse1;
+        _keys[2] = KeyCode.Mouse2;
+        _keys[3] = KeyCode.Mouse3;
+        _keys[4] = KeyCode.Mouse4;
+        _keys[5] = KeyCode.Mouse5;
+        _keys[6] = KeyCode.Mouse6;
+    }
+
     //once a button has been pressed, the "Press a button to play" text becomes yellow for 0.5 s
     //before returning white and then loading the next scene
     void Update()
     {
+        // Ignore any mouse button press
+        if (AnyMouseButton())
+        {
+            return;
+        }
+        
         if (Input.anyKeyDown)
         {
             text.color = Color.yellow;
             StartCoroutine(setTimeout());
         } 
+    }
+
+    private bool AnyMouseButton()
+    {
+        foreach (KeyCode key in _keys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                return true;
+            }
+        }
+
+        return false;
+
     }
 
     IEnumerator setTimeout()
